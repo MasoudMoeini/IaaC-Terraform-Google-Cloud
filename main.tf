085ee3ce847e001a70407e4bc06bb17d7dd6dd04
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-07-11 09:49:23 +0200

    update with flask app configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
-# Create a single Compute Engine instance
 provider "google" {
-  project = "`warm-rookery-325409`"
+  project = "{warm-rookery-325409}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
+
+# [START compute_flask_quickstart_vm]
+# Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
   name         = "flask-vm"
   machine_type = "f1-micro"
-  zone         = "us-west1-a"
+  zone         = "us-central1-c"
   tags         = ["ssh"]
 
   metadata = {
@@ -26,5 +30,49 @@ resource "google_compute_instance" "default" {
     access_config {
       # Include this section to give the VM an external IP address
     }
+  }
+}
+# [END compute_flask_quickstart_vm]
+
+# [START vpc_flask_quickstart_ssh_fw]
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = "default"
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+# [END vpc_flask_quickstart_ssh_fw]
+
+
+# [START vpc_flask_quickstart_5000_fw]
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+# [END vpc_flask_quickstart_5000_fw]
+
+# Create new multi-region storage bucket in the US
+# with versioning enabled
+
+# [START storage_bucket_tf_with_versioning]
+resource "google_storage_bucket" "default" {
+  name          = "bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
   }
 }
